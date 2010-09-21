915e0ccda016dff076e29ad62d3acbd9fa81c680
Daniel Langner <twobit@one.(none)>
2010-09-22 00:02:27 +0200

    map

---
+++ map.js
@@ -0,0 +1,25 @@
+
+
+var Map = function() {
+
+	this.data = [
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[1,1,1,1]
+	];
+
+};
+
+Map.prototype.draw = function() {
+
+	for(var y = 0, ly = this.data.length; y < ly; ++y) {
+		for(var x = 0, lx = this.data[y]; x < lx; ++x) {
+	
+	
+		}	
+	}
+
+};
+
+var map = new Map();
