628cb1af5f0b1f9dad2b9dc80ca10c4012cc0062
Daniel Langner <twobit@one.(none)>
2010-09-22 00:17:49 +0200

    map

--- map.js
+++ map.js
@@ -9,15 +9,20 @@ var Map = function() {
 		[1,1,1,1]
 	];
 
+	this.tileSize = 20;
+
 };
 
 Map.prototype.draw = function() {
 
+	ctx.fillStyle = "#777";
+
 	for(var y = 0, ly = this.data.length; y < ly; ++y) {
-		for(var x = 0, lx = this.data[y]; x < lx; ++x) {
-	
-	
-		}	
+		for(var x = 0, lx = this.data[y].length; x < lx; ++x) {
+
+			if(this.data[y][x]) ctx.fillRect(x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);
+
+		}
 	}
 
 };
